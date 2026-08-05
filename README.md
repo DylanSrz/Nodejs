9d3d6c74d5417c609e1c1d4e4f4484bf71358817
DylanSrz <dylansrz96@gmail.com>
2026-08-05 17:30:20 -0500

    websockets clase completa

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Nodejs
 Estructura de trabajo para Node.js ya completada y lista para desarrollar.
 Repositorio para practicar diferentes temas de desarrollo por ramas. Una rama un contexto diferente.
+
+
+# dependencias a instalar
+npm i express
+npm i socket.io
+npm i cors
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log(`Hola mundo.`)
+import express, { type Request, type Response }  from 'express';
+import http from 'http'
+import cors from 'cors'
+import { Server} from 'socket.io';
+
+const app = express()
+app.use(express.json())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+})
+
+io.on('connection', socket => {
+    console.log(`usuario conectado ${socket.id}`)
+})
+
+app.post('/send', (req: Request, res: Response) => {
+    
+    const {username, message, socketId} = req.body
+
+    io.except(socketId).emit ('send-message', {username, message})
+
+    res.json({message: 'mensaje enviado'})
+})
+
+server.listen(3000, () => {
+    console.log('server runing...')
+})
